5b904b50e13157847914ffee28599ab8000378cd
Piyushkashya <piyush8580kashyap@gmail.com>
2024-02-01 19:44:45 +0530

    utils + cn.ts

--- src/app/components/Search.tsx
+++ src/app/components/Search.tsx
@@ -2,22 +2,20 @@ import React, { HtmlHTMLAttributes } from "react";
 import { MdSearch } from "react-icons/md";
 
 type Props = {
-  className? : string
+  className?: string;
   value: string;
   onchange: React.ChangeEventHandler<HTMLInputElement> | undefined;
-  onsubmit: React.FormEventHandler<HTMLInputElement> | undefined
+  onsubmit: React.FormEventHandler<HTMLInputElement> | undefined;
 };
 
 const Search = (props: Props) => {
   return (
-    <form className="flex relative item-center  h-10">
+    <form onSubmit={props.onsubmit} className="flex relative item-center  h-10">
       <input
         className="rounded-md shadow-sm py-4 px-4 bg-slate-500"
         type="text"
         placeholder="Search"
-        // onClick={}
         onChange={props.onchange}
-        onSubmit={props.onsubmit}
         value={props.value}
       />
       <button>
