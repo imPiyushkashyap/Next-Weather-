b716f46fe01e1086bfa3e5d273bd04f30054ec09
Piyushkashya <piyush8580kashyap@gmail.com>
2024-02-02 20:32:39 +0530

    change in 14th line

--- src/app/components/Search.tsx
+++ src/app/components/Search.tsx
@@ -1,16 +1,17 @@
-import React, { HtmlHTMLAttributes } from "react";
+import React from "react";
 import { MdSearch } from "react-icons/md";
+import { cn } from "../utils/cn";
 
 type Props = {
   className?: string;
   value: string;
   onchange: React.ChangeEventHandler<HTMLInputElement> | undefined;
-  onsubmit: React.FormEventHandler<HTMLInputElement> | undefined;
+  onsubmit: React.FormEventHandler<HTMLFormElement> | undefined;
 };
 
 const Search = (props: Props) => {
   return (
-    <form onSubmit={props.onsubmit} className="flex relative item-center  h-10">
+    <form onSubmit={props.onsubmit} className={cn("flex relative item-center  h-10 ", props.className)}>
       <input
         className="rounded-md shadow-sm py-4 px-4 bg-slate-500"
         type="text"
