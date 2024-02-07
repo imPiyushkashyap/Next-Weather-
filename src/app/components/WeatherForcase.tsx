447367ef709faf40aebd1f7d778f559c538d31cb
Piyushkashya <piyush8580kashyap@gmail.com>
2024-02-07 19:57:45 +0530

    minor change

--- src/app/components/WeatherForcase.tsx
+++ src/app/components/WeatherForcase.tsx
@@ -1,18 +1,35 @@
 import React from "react";
+import { GoEye } from "react-icons/go";
 
-type Props = {};
+interface details {
+  visibility: string;
+  humidity: string;
+  windspeed: string;
+  sunrise: string;
+  sunset: string;
+  airpressure: string;
+}
 
-export interface singleWeather {
+export interface SingleWeather {
   information: string;
   icon: React.ReactNode;
   value: string;
 }
-export default function WeatherForcase({}: Props) {
-  return <>WeatherDetail</>;
+
+export default function WeatherForecast(props: details) {
+  return (
+    <>
+      <SingleWeather icon={<GoEye />} information="visibility" value={props.visibility} />
+    </>
+  );
 }
 
-function singleWeather(props: singleWeather) {
-  return <div className="flex flex-col justify-between gap-2 items-center text-xs font-semibold text-black">
-    <p className=" whitespace-nowrap">{props.information}</p>
-  </div>;
+function SingleWeather(props: SingleWeather) {
+  return (
+    <div className="flex flex-col justify-between gap-2 items-center text-xs font-semibold text-black">
+      <p className="whitespace-nowrap">{props.information}</p>
+      <div className="text-3xl">{props.icon}</div>
+      <p>{props.value}</p>
+    </div>
+  );
 }
