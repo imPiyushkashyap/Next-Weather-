94dfec2d4633d8e065eb6a7959eae4f1df6be1b4
Piyushkashya <piyush8580kashyap@gmail.com>
2024-02-16 23:43:38 +0530

    updated DatWeather component

--- src/app/components/DayWeather.tsx
+++ src/app/components/DayWeather.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Container from "./Container";
-import { details } from "./WeatherForcase";
+import WeatherForecast, { details } from "./WeatherForcase";
 import WeatherIcon from "./WeatherIcon";
 import { F2C } from "../utils/fahrenheitTOcelsius";
 
@@ -18,31 +18,35 @@ interface DayWeatherProps extends details {
 export default function DayWeather(props: DayWeatherProps) {
   const {
     weatherIcon = "02d",
-    date,
-    day,
+    date = "16/02/2024",
+    day = "Friday",
     temp,
     feels_like,
     temp_min,
     temp_max,
-    description,
+    description = props.description
   } = props;
 
   return (
     <Container className="gap-4">
       <section className="flex gap-4 items-center px-4">
         <div>
-          <WeatherIcon iconName={weatherIcon} />
+          {/* <WeatherIcon iconName={weatherIcon} /> */}
           <p>{date}</p>
           <p className="text-sm">{day}</p>
         </div>
         <div className="felx flex-col px-4">
-          <span className="text-5xl">{F2C(temp ?? 0)} °</span>
+          <span className="text-4xl">{F2C(temp ?? 0)} °</span>
           <p className="text-xs space-x-1 whitespace-nowrap">
             <span>Feels Like</span>
             <span>{F2C(feels_like ?? 0)} °</span>
           </p>
+          <p className=" capitalize">Clear Sky</p>
         </div>
        
+      </section>
+      <section className=" overflow-x-auto flex justify-between gap-4 px-4 w-full pr-10">
+        <WeatherForecast {...props}/>
       </section>
     </Container>
   );
